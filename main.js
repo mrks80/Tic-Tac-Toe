2306da2dea287e2239ba2768000cae14e23663ae
mrks80 <loma80@gmx.net>
2022-07-25 21:25:42 +0200

    edited html/css/js

--- main.js
+++ main.js
@@ -34,8 +34,41 @@ function draw() {
 }
 
 function checkForWin() {
+
+    let winner;
+
+    // First row horizontal 
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
+        document.getElementById('line-1').style.transform = 'scaleX(1)';
+    }
+    // Second row horizontal 
+    if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
+        winner = fields[3];
+    }
+    // Third row horizontal 
+    if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
+        winner = fields[6];
+    }
+    // First row vertical
+    if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
+        winner = fields[0];
+    }
+    // Second row vertical
+    if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
+        winner = fields[1];
+    }
+    // Third row vertical 
+    if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
+        winner = fields[2];
+    }
+    // First row diagonal
+    if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
+        winner = fields[0];
+    }
+    // Second row diagonal 
+    if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
+        winner = fields[2];
     }
 
     if (winner) {
